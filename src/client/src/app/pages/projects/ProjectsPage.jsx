80f92e537040fd48d5c78cbe7b35ad83d526fd61
Owen De Waele <owendewa@student.arteveldehs.be>
2019-05-29 12:18:08 +0200

    Builded client

--- src/client/src/app/pages/projects/ProjectsPage.jsx
+++ src/client/src/app/pages/projects/ProjectsPage.jsx
@@ -32,6 +32,7 @@ import EasyFlexCol from "../../components/structural-components/flexbox/easy-fle
 class ProjectsPage extends Component {
   state = {
     projects: [],
+    projectImage: null,
     pagination: {
       limit: 5,
       page: 1,
@@ -46,7 +47,7 @@ class ProjectsPage extends Component {
 
   loadPosts = pageIndex => {
     console.log(pageIndex);
-    Api.findAllProjects({ limit: 3, skip: pageIndex })
+    Api.findAllProjects({ limit: 4, skip: pageIndex })
       .then(data => {
         console.log(data.docs);
         const prevProjects = this.state.projects;
@@ -61,8 +62,7 @@ class ProjectsPage extends Component {
             total: data.total
           }
         }));
-      })
-      .catch(error => {
+      }).catch(error => {
         console.log(error);
       });
   };
@@ -109,6 +109,7 @@ class ProjectsPage extends Component {
             text="Wat als je drie studententeams in verschillende landen dezelfde briefing geeft? En ze vervolgens parallel aan hun eigen oplossing laat werken? In het initiatief Parkspot besloten docenten van de Hogeschool van Amsterdam, Hochschulde der Medien (Stuttgart) en Arteveldehogeschool om dat eens uit te testen."
             style="standard-text-paragraph par-pos-1 paragraph-mb-med"
           />
+           {this.items}
           <BlankDiv style="blank-div-lg" />
           <EasyFlexRow style="row-space-between col-card-width-1 ">
             <EasyFlexCol style="col-space-between col-card-width-full">
@@ -128,11 +129,6 @@ class ProjectsPage extends Component {
           <Footer />
         </GridWrapper>
       </React.Fragment>
-
-      /* <React.Fragment>
-        <Title style="section-title" text="Projects" />
-           {this.items}
-      </React.Fragment> */
     );
   }
 }
