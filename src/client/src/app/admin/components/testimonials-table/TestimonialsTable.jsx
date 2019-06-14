1a7bb3cd9c0729af4ad84289cebf533fb2ef4595
Owen De Waele <owendewa@student.arteveldehs.be>
2019-06-14 15:44:38 +0200

    created testimonial table

---
+++ src/client/src/app/admin/components/testimonials-table/TestimonialsTable.jsx
@@ -0,0 +1,222 @@
+
+
+/*
+External libraries
+*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Enum from "es6-enum";
+
+/*
+Material UI
+*/
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import IconCreate from '@material-ui/icons/Create';
+import IconDelete from '@material-ui/icons/Delete';
+import IconDeleteForever from '@material-ui/icons/DeleteForever';
+import Paper from '@material-ui/core/Paper';
+
+const POSTACTIONSENUM = Enum('DELETE', 'SOFTDELETE', 'SOFTUNDELETE');
+
+/*
+Styles
+*/
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+  },
+});
+
+class TestimonialsTable extends Component {
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+  state = {
+    testimonials: null,
+    imageId: null,
+    postAction: null,
+    dialogOpen: false,
+    dialogTitle: '',
+    dialogMessage: ''
+  };
+
+  
+
+  handleDialogOpen = (imageId, postAction) => {
+    let title = '';
+    let message = '';
+
+    switch(postAction) {
+      case POSTACTIONSENUM.DELETE:
+        title = 'Delete from the database?';
+        message= `Do you wish permenantly delete the post with id ${imageId}?`;
+        break;
+      case POSTACTIONSENUM.SOFTDELETE:
+        title = 'Soft-delete from the database?';
+        message= `Do you wish to soft-delete the post with id ${imageId}?`;
+        break;
+      case POSTACTIONSENUM.SOFTUNDELETE:
+        title = 'Soft-undelete from the database?';
+        message= `Do you wish to soft-undelete the post with id ${imageId}?`;
+        break;
+    }
+
+    this.setState({
+      imageId: imageId,
+      postAction: postAction,
+      dialogOpen: true,
+      dialogTitle: title,
+      dialogMessage: message
+    });
+  };
+
+  handleDialogClose = () => {
+    this.setState({dialogOpen: false});
+  };
+
+  handleDialogSubmit = () => {
+    let url = '';
+    let options = {};
+
+    switch(this.state.postAction) {
+      case POSTACTIONSENUM.DELETE:
+        url = `/api/v1/testimonials/${this.state.imageId}`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+      case POSTACTIONSENUM.SOFTDELETE:
+        url = `/api/v1/testimonials/${this.state.imageId}?mode=softdelete`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+      case POSTACTIONSENUM.SOFTUNDELETE:
+        url = `/api/v1/testimonials/${this.state.imageId}?mode=softundelete`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+    }
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(results => {
+        if(results.mode && results.mode === 'delete') {
+          this.loadTestimonials();
+        } else {
+          const testimonial = results.testimonial;
+          const i = this.state.testimonials.findIndex((obj, index, array) => {
+            return obj._id === testimonial._id;
+          });
+          const testimonials = this.state.testimonials;
+          testimonials[i] = testimonial;
+  
+          this.setState(prevState => ({
+            ...prevState,
+            testimonials: testimonials
+          }));
+        }
+        }
+      );
+
+    this.handleDialogClose();
+  }
+
+  componentWillMount() {
+    this.loadTestimonials();
+  }
+
+  loadTestimonials = () => {
+    fetch('/api/v1/testimonials')
+      .then( response => response.json())
+      .then( item => this.setState({ testimonials: item })); 
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { testimonials } = this.state;
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <TableHead>
+              <TableRow>
+                <TableCell>Subject</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Body</TableCell>
+                <TableCell>Type</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {testimonials && testimonials.map( (testimonial, index) => (
+                <TableRow key={testimonial.id}>
+                  <TableCell>{testimonial.subject}</TableCell>
+                  <TableCell>{testimonial.name}</TableCell>
+                  <TableCell>{testimonial.body}</TableCell>
+                  <TableCell>{testimonial.typeId}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      component={Link} to={ `/admin/testimonials/${testimonial.id}/edit`}>
+                      <IconCreate />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => this.handleDialogOpen(testimonial.id, (testimonial.deleted_at)?POSTACTIONSENUM.SOFTUNDELETE:POSTACTIONSENUM.SOFTDELETE)} style={{ opacity: ((testimonial.deleted_at)?0.3:1) }}>
+                      <IconDelete/>
+                    </IconButton>
+                    <IconButton
+                      onClick={() => this.handleDialogOpen(testimonial.id, POSTACTIONSENUM.DELETE)}>
+                      <IconDeleteForever />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <Dialog
+          fullScreen={false}
+          open={this.state.dialogOpen}
+          onClose={this.handleDialogClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">{this.state.dialogTitle}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {this.state.dialogMessage}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleDialogClose()} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => this.handleDialogSubmit()} color="primary" autoFocus>
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Paper>
+    );  
+  }
+}
+
+export default withStyles(styles)(TestimonialsTable);
