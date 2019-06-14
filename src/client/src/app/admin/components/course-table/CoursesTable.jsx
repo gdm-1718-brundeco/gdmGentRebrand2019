9862a983be28353512e0f50cf04f889c48add654
Owen De Waele <owendewa@student.arteveldehs.be>
2019-06-14 15:43:31 +0200

    created courses table

---
+++ src/client/src/app/admin/components/course-table/CoursesTable.jsx
@@ -0,0 +1,221 @@
+
+/*
+External libraries
+*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Enum from "es6-enum";
+
+/*
+Material UI
+*/
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import IconCreate from '@material-ui/icons/Create';
+import IconDelete from '@material-ui/icons/Delete';
+import IconDeleteForever from '@material-ui/icons/DeleteForever';
+import Paper from '@material-ui/core/Paper';
+
+const POSTACTIONSENUM = Enum('DELETE', 'SOFTDELETE', 'SOFTUNDELETE');
+
+/*
+Styles
+*/
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+  },
+});
+
+class CoursesTable extends Component {
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+  state = {
+    courses: null,
+    courseId: null,
+    postAction: null,
+    dialogOpen: false,
+    dialogTitle: '',
+    dialogMessage: ''
+  };
+
+  
+
+  handleDialogOpen = (courseId, postAction) => {
+    let title = '';
+    let message = '';
+
+    switch(postAction) {
+      case POSTACTIONSENUM.DELETE:
+        title = 'Delete from the database?';
+        message= `Do you wish permenantly delete the post with id ${courseId}?`;
+        break;
+      case POSTACTIONSENUM.SOFTDELETE:
+        title = 'Soft-delete from the database?';
+        message= `Do you wish to soft-delete the post with id ${courseId}?`;
+        break;
+      case POSTACTIONSENUM.SOFTUNDELETE:
+        title = 'Soft-undelete from the database?';
+        message= `Do you wish to soft-undelete the post with id ${courseId}?`;
+        break;
+    }
+
+    this.setState({
+      courseId: courseId,
+      postAction: postAction,
+      dialogOpen: true,
+      dialogTitle: title,
+      dialogMessage: message
+    });
+  };
+
+  handleDialogClose = () => {
+    this.setState({dialogOpen: false});
+  };
+
+  handleDialogSubmit = () => {
+    let url = '';
+    let options = {};
+
+    switch(this.state.postAction) {
+      case POSTACTIONSENUM.DELETE:
+        url = `/api/v1/courses/${this.state.courseId}`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+      case POSTACTIONSENUM.SOFTDELETE:
+        url = `/api/v1/courses/${this.state.courseId}?mode=softdelete`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+      case POSTACTIONSENUM.SOFTUNDELETE:
+        url = `/api/v1/courses/${this.state.courseId}?mode=softundelete`;
+        options = {
+          method: 'DELETE'
+        }
+        break;
+    }
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(results => {
+        if(results.mode && results.mode === 'delete') {
+          this.loadCourses();
+        } else {
+          const course = results.course;
+          const i = this.state.courses.findIndex((obj, index, array) => {
+            return obj._id === course._id;
+          });
+          const courses = this.state.courses;
+          courses[i] = course;
+  
+          this.setState(prevState => ({
+            ...prevState,
+            courses: courses
+          }));
+        }
+        }
+      );
+
+    this.handleDialogClose();
+  }
+
+  componentWillMount() {
+    this.loadCourses();
+  }
+
+  loadCourses = () => {
+    fetch('/api/v1/courses')
+      .then( response => response.json())
+      .then( item => this.setState({ courses: item })); 
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { courses } = this.state;
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Points</TableCell>
+                <TableCell>Year</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {courses && courses.map( (course, index) => (
+                <TableRow key={course.id}>
+                  <TableCell>{course.name}</TableCell>
+                  <TableCell>{course.description}</TableCell>
+                  <TableCell>{course.points}</TableCell>
+                  <TableCell>{course.year}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      component={Link} to={ `/admin/courses/${course.id}/edit`}>
+                      <IconCreate />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => this.handleDialogOpen(course.id, (course.deleted_at)?POSTACTIONSENUM.SOFTUNDELETE:POSTACTIONSENUM.SOFTDELETE)} style={{ opacity: ((course.deleted_at)?0.3:1) }}>
+                      <IconDelete/>
+                    </IconButton>
+                    <IconButton
+                      onClick={() => this.handleDialogOpen(course.id, POSTACTIONSENUM.DELETE)}>
+                      <IconDeleteForever />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <Dialog
+          fullScreen={false}
+          open={this.state.dialogOpen}
+          onClose={this.handleDialogClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">{this.state.dialogTitle}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {this.state.dialogMessage}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleDialogClose()} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => this.handleDialogSubmit()} color="primary" autoFocus>
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Paper>
+    );  
+  }
+}
+
+export default withStyles(styles)(CoursesTable);
